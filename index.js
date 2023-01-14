ed75504d673a6467219a2c6f37980267b54fb90d
A-Abd <abrar.abdulwahed@gmail.com>
2023-01-15 00:38:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function superbowlWin(records) {
     let win =  records.find(elem => elem.result === "W");
-    if(win !== undefined)
-        return win.year;
-    return win
+    // if(win !== undefined)
+    //     return win.year;
+    // return win
+    return win ? win.year: undefined;
 }
